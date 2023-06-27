2512e29b1f29ad60422c4f467d439aa347ef5f42
chaunhatquang <info.quangnhat@gmail.com>
2023-06-27 17:26:10 +0700

    add loadingindicator and getCurrentYearAndMonth function

--- Utils/GetCurrentYearAndMonth.tsx
+++ Utils/GetCurrentYearAndMonth.tsx
@@ -6,33 +6,39 @@ const config = require("../Config/config.json");
 const URL = config.BASE_URL;
 
 export const getCurrentMonthAndYearId = async () => {
-    try {
-      // Tải dữ liệu years từ API
-      const yearsResponse : any = await fetchData(URL,paramsYear);
-      const years = yearsResponse.data as IYear[];
-  
-      // Tải dữ liệu months từ API
-      const monthsResponse : any = await fetchData(URL,paramsMonth);
-      const months = monthsResponse.data as IMonth[];
-  
-      // Lấy ngày tháng hiện tại
-      const currentDate = new Date();
-      const currentYear = currentDate.getFullYear();
-      const currentMonth = currentDate.getMonth() + 1; // Tháng trong JavaScript bắt đầu từ 0, nên cần +1 để đúng với danh sách months
-  
-      // Tìm id của năm hiện tại trong danh sách years
-      const currentYearId = years.find(year => year.nambc === currentYear.toString())?.id;
-  
-      // Tìm id của tháng hiện tại trong danh sách months
-      const currentMonthId = months.find(month => month.thang.includes(currentMonth.toString()))?.id;
-  
-      return {
-        yearId: currentYearId,
-        monthId: currentMonthId
-      };
-    } catch (error) {
-      console.error('Error:', error);
-      return null;
-    }
-  };
+  try {
+    // Tải dữ liệu years từ API
+    const yearsResponse: any = await fetchData(URL, paramsYear);
+    const years = yearsResponse.data as IYear[];
+
+    // Tải dữ liệu months từ API
+    const monthsResponse: any = await fetchData(URL, paramsMonth);
+    const months = monthsResponse.data as IMonth[];
+
+    // Lấy ngày tháng hiện tại
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1; // Tháng trong JavaScript bắt đầu từ 0, nên cần +1 để đúng với danh sách months
+
+    // Tìm id và tên của năm hiện tại trong danh sách years
+    const currentYearData = years.find(year => year.nambc === currentYear.toString());
+    const currentYearId = currentYearData?.id;
+    const currentYearName = currentYearData?.nambc;
+
+    // Tìm id và tên của tháng hiện tại trong danh sách months
+    const currentMonthData = months.find(month => month.thang.includes(currentMonth.toString()));
+    const currentMonthId = currentMonthData?.id;
+    const currentMonthName = currentMonthData?.thang;
+
+    return {
+      yearId: currentYearId,
+      monthId: currentMonthId,
+      yearName: currentYearName,
+      monthName: currentMonthName,
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+};
 
